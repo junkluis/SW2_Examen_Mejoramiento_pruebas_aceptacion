62be5250dfdfb84c8d655d7ad0368f988fb944a6
wmartine32 <wellinmart32@gmail.com>
2019-09-09 09:38:36 -0500

    Examen

--- features/cobroPasaje.feature
+++ features/cobroPasaje.feature
@@ -3,9 +3,10 @@
 Característica: Cobrar pasaje
 
  @jornadasDeIngreso
-  Escenario: Nombre del escenario
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
+  Escenario: Estudiante pagagando pasaje exitoso
+      Dado soy estudiante de ESPOL con carnet en la ruta "NORTE"
+      Dado que dispongo de 50 en mi carnet estudiantil y el torniquete dispone de conexión a internet
+      Cuando acerque mi carnet de ESPOL al torniquete
       Entonces genera el siguiete resultado 'resultado_variable'
       Y tambien ocurre lo siguiente (si es necesario)
 
--- features/steps/cobrar_pasaje_steps.py
+++ features/steps/cobrar_pasaje_steps.py
@@ -6,20 +6,28 @@ from src.cobrar_pasaje import *
 def before_scenario(context, scenario):
 	context = {}
 
+@given("soy estudiante de ESPOL con carnet en la ruta {ruta_viaje}"):
+def step_impl(context, carnet):
+	context.carnet_estudiantil = {"codigo":000000000, "saldo":20}
+	context.ruta_viaje = ruta_viaje
 
-@given("que se cumplen los requisitos")
+@given("que dispongo de {total_saldo} en mi carnet estudiantil y el torniquete dispone de conexión a internet")
+def step_impl(context, total_saldo):
+	context.total_saldo = total_saldo
+
+@when("acerque mi carnet de ESPOL al torniquete")
 def step_impl(context):
-	pass
+	if context.ruta_viaje == "NORTE":
+		ruta_seleccionada = "NORTE"
+		carnet_estudiantil = context.carnet_estudiantil
+		mensaje_error = cobrar_pasaje_ruta(ruta_seleccionada, carnet_estudiantil)
+		context.mensaje_error = mensaje_error
 
-@when("se ejecute una accion")
-def step_impl(context):
-	pass
-
-@then("genera el siguiete resultado '{variable}'")
+@then("se me escontará '{variable}'")
 def step_impl(context, variable):
 	print(variable)
-	pass
+	context.total_saldo = context.total_saldo - variable
 
-@then("tambien ocurre lo siguiente (si es necesario)")
-def step_impl(context):
-	pass
+@then("me presentará el mensaje: {mensaje_error}")
+def step_impl(context, mensaje):
+	assert context.mensaje_error == mensaje
