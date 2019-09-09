dc88fdbd0c2c1ea8d7f9e7ac1540b2621e52da77
Lesther Carranza <ldcarran@espol.edu.ec>
2019-09-09 09:35:47 -0500

    Primer feature

--- features/cobroPasaje.feature
+++ features/cobroPasaje.feature
@@ -3,10 +3,12 @@
 Característica: Cobrar pasaje
 
  @jornadasDeIngreso
-  Escenario: Nombre del escenario
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
-      Entonces genera el siguiete resultado 'resultado_variable'
-      Y tambien ocurre lo siguiente (si es necesario)
+  Escenario: Pago de pasaje exitoso
+      Dado dispongo de mi carnet de estudiante con codigo 201414466 y $0.75 saldo
+      Y estoy tomando la ruta CENTRO
+      Y el torniquete si tiene conexion a internet
+      Cuando pase el carnet por el torniquete
+      Entonces se debita de mi saldo en base a mi ruta
+      Y se me muestra mi saldo restante como $0.50
 
 
--- features/steps/cobrar_pasaje_steps.py
+++ features/steps/cobrar_pasaje_steps.py
@@ -4,22 +4,54 @@ from src.cobrar_pasaje import *
 
 #Condiciones antes de empezar cualquier STEP
 def before_scenario(context, scenario):
-	context = {}
+        """ Settear el contexto necesario a tener para cada escenario"""        
+        context = {}
 
 
-@given("que se cumplen los requisitos")
+@given("dispongo de mi carnet de estudiante con codigo {codigo} y ${saldo} saldo")
+def step_impl(context, codigo, saldo):
+        """Llena la informacion relevante del carnet estudiantil"""        
+        context.carnet = {}
+        context.carnet["codigo"] = codigo
+        context.carnet["saldo"] = float(saldo)
+
+@given("estoy tomando la ruta {ruta}")
+def step_impl(context, ruta):
+        """ especifica la ruta tomada """
+        context.ruta = ruta
+
+
+@given("el torniquete {conexion} tiene conexion a internet")
+def step_impl(context, conexion):
+        """ indica si hay conextion a internet para el torniquete """
+        conexion = conexion.lower()
+        if conexion == "si":
+            context.conexion = True
+        elif conexion == "no":
+            context.conexion = False
+        else:
+            conexion = None
+        assert(conexion != None)
+
+
+@when("pase el carnet por el torniquete")
 def step_impl(context):
-	pass
+        """ realiza el cobro de saldo """
+        resultado = cobrar_pasaje_ruta(context.ruta,
+                                        context.carnet,
+                                        context.conexion)
+        context.resultado = resultado
 
-@when("se ejecute una accion")
+
+@then("se debita de mi saldo en base a mi ruta")
 def step_impl(context):
-	pass
+        """ actualiza el saldo del carnet """
+        assert(type(context.resultado) == float)
+        context.carnet['saldo'] = context.resultado
 
-@then("genera el siguiete resultado '{variable}'")
-def step_impl(context, variable):
-	print(variable)
-	pass
 
-@then("tambien ocurre lo siguiente (si es necesario)")
-def step_impl(context):
-	pass
+@then("se me muestra mi saldo restante como ${saldo_restante}")
+def step_impl(context, saldo_restante):
+        """ se muestra el resultado de la transaccion"""
+        assert(context.carnet['saldo'] == float(saldo_restante))
+        print("Su saldo es $%s" % saldo_restante)
