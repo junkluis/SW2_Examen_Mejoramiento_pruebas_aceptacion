7f7933f8ee8617c1f8f104b85d1b7644779302e5
AnibalG <aagamboa@espol.edu.ec>
2019-09-09 09:54:16 -0500

    pylint

--- features/steps/cobrar_pasaje_steps.py
+++ features/steps/cobrar_pasaje_steps.py
@@ -2,40 +2,45 @@ from behave import *
 from src.cobrar_pasaje import *
 
 
-#Condiciones antes de empezar cualquier STEP
+# Condiciones antes de empezar cualquier STEP
 def before_scenario(context, scenario):
-	context = {}
+    context = {}
 
 
 @given("que se tiene '{saldo}' dolares asociados al carnet estudiantil con codigo '{codigo}'")
 def step_impl(context, saldo, codigo):
-	carnet = {"codigo": int(codigo), "saldo": float(saldo)}
-	context.carnet= carnet
+    carnet = {"codigo": int(codigo), "saldo": float(saldo)}
+    context.carnet = carnet
 
 
 @given("se tiene conexión a internet")
 def step_impl(context):
     context.conexion = True
 
+
 @when("se acerque el carnet al torniquete")
 def step_impl(context):
-	ruta = "NORTE"
-	result = cobrar_pasaje_ruta(ruta, context.carnet, context.conexion)
-	context.result = result
+    ruta = "NORTE"
+    result = cobrar_pasaje_ruta(ruta, context.carnet, context.conexion)
+    context.result = result
+
 
 @then("se descontará el costo del pasaje")
 def step_impl(context):
-	pass
+    pass
+
 
 @then("mostara el mensaje: '{mensaje}'")
 def step_impl(context, mensaje):
-	print(context.result)
-	assert(mensaje == context.result)
+    print(context.result)
+    assert(mensaje == context.result)
+
 
 @then("se mostrará el siguiente costo actualizado : '{monto}'")
 def step_impl(context, monto):
-	assert(float(monto) == context.result)
+    assert(float(monto) == context.result)
+
 
 @then("se mostrará el siguiente saldo actual : '{monto}'")
 def step_impl(context, monto):
-	assert(float(monto) == context.carnet["saldo"])
+    assert(float(monto) == context.carnet["saldo"])
