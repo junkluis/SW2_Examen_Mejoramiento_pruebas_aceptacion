acd20f6d46ecd3bb7b497cbbef75eb2b3e32e09f
Miguel Sanchez <mangelsr@live.com>
2019-09-09 09:42:25 -0500

    [ADD] Test & refactorigin

--- features/cobroPasaje.feature
+++ features/cobroPasaje.feature
@@ -3,10 +3,20 @@
 Característica: Cobrar pasaje
 
  @jornadasDeIngreso
-  Escenario: Nombre del escenario
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
-      Entonces genera el siguiete resultado 'resultado_variable'
-      Y tambien ocurre lo siguiente (si es necesario)
+  Escenario: Pago exitoso
+      Dado que dispongo de dinero asociado a mi carnet estudiantil
+      Y el torniquete dispone de conexion a internet
+      Cuando acerque mi carnet de ESPOL al torniquete
+      Entonces se me descontara el pasaje
+      Y me presentara '1.7'
+
+
+@jornadasDeIngreso
+  Escenario: Pago insuficiente
+      Dado que no dispongo de dinero asociado a mi carnet estudiantil
+      Y el torniquete dispone de conexion a internet
+      Cuando acerque mi carnet de ESPOL al torniquete
+      Entonces aparecera el siguiente mensaje 'No dispone del monto suficiente'
+      Y me presentara '0'
 
 
--- features/steps/cobrar_pasaje_steps.py
+++ features/steps/cobrar_pasaje_steps.py
@@ -2,24 +2,65 @@ from behave import *
 from src.cobrar_pasaje import *
 
 
-#Condiciones antes de empezar cualquier STEP
+# Condiciones antes de empezar cualquier STEP
 def before_scenario(context, scenario):
-	context = {}
+    context = {}
 
 
-@given("que se cumplen los requisitos")
+@given("que dispongo de dinero asociado a mi carnet estudiantil")
 def step_impl(context):
-	pass
+    student_id = {
+        'codigo': '201505935',
+        'saldo': 2
+    }
+    context.student_id = student_id
 
-@when("se ejecute una accion")
+
+@given("que no dispongo de dinero asociado a mi carnet estudiantil")
 def step_impl(context):
-	pass
+    student_id = {
+        'codigo': '201505935',
+        'saldo': 0
+    }
+    context.student_id = student_id
+
+
+@given("el torniquete dispone de conexion a internet")
+def step_impl(context):
+    context.conection = True
+
+
+@when("acerque mi carnet de ESPOL al torniquete")
+def step_impl(context):
+    if context.student_id['saldo'] >= 2:
+        actual_balance = cobrar_pasaje_ruta(
+            'NORTE', context.student_id, context.conection)
+        context.actual_balance = actual_balance
+    else:
+        error_msg = cobrar_pasaje_ruta(
+            'NORTE', context.student_id, context.conection)
+        context.error_msg = error_msg
+
 
 @then("genera el siguiete resultado '{variable}'")
 def step_impl(context, variable):
-	print(variable)
-	pass
+    print(variable)
+    pass
 
-@then("tambien ocurre lo siguiente (si es necesario)")
+
+@then("se me descontara el pasaje")
 def step_impl(context):
-	pass
+    pass
+
+
+@then("me presentara '{actual_balance}'")
+def step_impl(context, actual_balance):
+    if str(context.scenario) == "Pago exitoso":
+        assert(context.actual_balance, float(actual_balance))
+    elif str(context.scenario) == "Pago insuficiente":
+        assert(context.student_id['saldo'], float(actual_balance))
+
+
+@then("aparecera el siguiente mensaje '{msg}'")
+def step_impl(context, msg):
+    assert(context.error_msg, msg)
