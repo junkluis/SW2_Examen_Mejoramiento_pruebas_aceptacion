e8cb22e8a0498e0d534fc48fc78ab05853d2f010
Alfonso Pulido Carrera <32580083+alfonsopulido13@users.noreply.github.com>
2019-09-09 12:46:39 -0500

    aceptacion Alfonso Pulido 3
    aceptacion Alfonso Pulido 3

--- features/steps/cobrar_pasaje_steps.py
+++ features/steps/cobrar_pasaje_steps.py
@@ -1,43 +1,39 @@
 from behave import *
 from src.cobrar_pasaje import *
 
-
 #Condiciones antes de empezar cualquier STEP
-def before_scenario(context, scenario):
-	context = {}
-
+def before_scenario(context, scenario): context = {}
 
 @given("se cumplen los requisitos")
-def step_impl(context):
-	pass
+def step_impl(context): pass
 
 @when("ejecutar una accion")
-def step_impl(context):
-	pass
+def step_impl(context): pass
 
 @then("genera el siguiente resultado '{variable}'")
-def step_impl(context, variable):
-	print(variable)
+def step_impl(context, variable): print(variable)
 	pass
 
 @then("tambien ocurre (si es necesario)")
-def step_impl(context):
-	pass
+def step_impl(context):pass
 
-@given("que tengo ${dolares} dólares en mi carnet de codigo {codigo}")
+#tema 1
+
+@given("que tengo ${dolares} dolares en mi carnet de codigo {codigo}")
 def saldo_codigo(context, dolares, codigo):
 	carnet = {"codigo": codigo, "saldo":float(dolares)}
 	context.carnet = carnet
 	print(carnet)
 
-@when("ponga mi carnet en el torniquete de la RUTA {ruta} y este {internet} tenga Internet")
+@when("ponga mi carnet en el torniquete de la RUTA {ruta} y este {internet} tenga internet")
 def ruta_internet(context, ruta, internet):
 	context.ruta = ruta
 	context.internet = internet == "si"
 
 @then("se descontará el pasaje según la ruta que escogí")
 def descontarPasaje(context):
-	retorno = cobrar_pasaje_ruta(context.ruta, context.carnet, context.internet)
+	retorno = 
+	_pasaje_ruta(context.ruta, context.carnet, context.internet)
 	assert retorno == context.carnet["saldo"]
 
 @then("se presentará el nuevo saldo: ${saldo}")
